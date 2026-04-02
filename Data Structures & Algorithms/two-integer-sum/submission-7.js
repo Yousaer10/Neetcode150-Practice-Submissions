51ff5cb74cdf7bdcbc3e74d4332b77ee1adcc84b
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-04-02 11:11:35 +0100

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.js
@@ -0,0 +1,14 @@
+class Solution {
+    twoSum(nums, target) {
+        const complement =  new Map();
+
+        for(let i = 0; i < nums.length; i++) {
+            let diff = target - nums[i];
+            if(complement.has(diff)) {
+                return [complement.get(diff), i];
+            } else {
+                complement.set(nums[i], i);
+            }
+        }
+    }
+}
