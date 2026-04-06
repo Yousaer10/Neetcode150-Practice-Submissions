07df3e259b088efb0bafec9015c395a1f80ddf76
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-04-06 02:30:31 +0100

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.js
@@ -0,0 +1,19 @@
+class Solution {
+    search(nums, target) {
+        let low = 0;
+        let high = nums.length - 1;
+        let mid;
+
+        while(low <= high) {
+            mid = Math.floor((high + low) / 2);
+            if(target === nums[mid]) {
+                return mid;
+            } else if(target > nums[mid]) {
+                low = mid + 1;
+            } else if(target < nums[mid]) {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
