cc5a8b427cfdf2d613a4b7094cea64df0394e876
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-04-08 19:50:14 +0100

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) {
+            return false;
+        }
+        let count = new Map();
+
+        for(let char of s) {
+            count.set(char, (count.get(char) ?? 0) + 1);
+        }
+
+        for(let char of t) {
+            if(!count.has(char)) {
+                return false;
+            }
+            count.set(char, (count.get(char)) - 1);
+            
+            if(count.get(char) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
