15845b9abbd1891a1af65aba0f62f68440cff18d
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-04-08 11:13:12 +0100

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.js
@@ -0,0 +1,14 @@
+class Solution {
+    hasDuplicate(nums) {
+        const seen = new Set();
+
+        for(let num of nums) {
+            if(seen.has(num)) {
+                return true;
+            } else {
+                seen.add(num);
+            }
+        }
+        return false;
+    }
+}
