76c3fa107843835bf22bbeca4d0b9f5aacb1cc3c
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-04-09 09:29:21 +0100

    Add: merge-two-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-2.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    mergeTwoLists(list1, list2) {
+        let dummy = new ListNode(-1);
+        let curr = dummy;
+        
+        while(list1 !== null && list2 !== null) {
+            if(list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = list1 !== null ?  list1 : list2;
+        return dummy.next;
+    }
+}
