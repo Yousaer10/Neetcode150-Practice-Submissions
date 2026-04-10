0ad63e9ee33306a0061fdfdf4d6ad57f090562d7
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-04-10 09:01:15 +0100

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.js
@@ -0,0 +1,16 @@
+class Solution {
+    isPalindrome(s) {
+        let palindrome = s.toLowerCase().replace(/[^a-z0-9]/g,"");
+        let left = 0;
+        let right = palindrome.length - 1;
+
+        while(left < right) {
+            if(palindrome[left] !== palindrome[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
