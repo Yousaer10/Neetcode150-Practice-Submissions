7ceaae32523cb78fd03cdf126ff276d01457453e
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-04-11 08:58:33 +0100

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.js
@@ -0,0 +1,18 @@
+class Solution {
+    maxProfit(prices) {
+        let minPrice = Infinity;
+        let currentProfit = 0;
+        let maxProfit = 0;
+
+        for(let i = 0; i < prices.length; i++) {
+            if(prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                currentProfit = prices[i] - minPrice;
+            } if(maxProfit < currentProfit) {
+                maxProfit = currentProfit;
+            }
+        }
+        return maxProfit;
+    }
+}
