f50267b43b363ee0c0997875ebf02749669d7420
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-04-14 18:40:44 +0100

    Update: same-binary-tree - submission-0

--- Data Structures & Algorithms/same-binary-tree/submission-0.js
+++ Data Structures & Algorithms/same-binary-tree/submission-0.js
@@ -13,7 +13,6 @@ class Solution {
     isSameTree(p, q) {
         if(!p && !q) return true;
         if(!p || !q || p.val !== q.val) return false;
-        
         return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right);
     }
 }
