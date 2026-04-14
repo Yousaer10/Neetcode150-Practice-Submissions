32138d596a9ba7514db2efe4404167b46ed43900
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-04-14 10:39:37 +0100

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    isBalanced(root) {
+        const dfs = (node) => {
+            if(!node) return 0;
+            let leftHeight = dfs(node.left);
+            let rightHeight = dfs(node.right);
+
+            if(leftHeight === -1 || rightHeight === -1) {
+                return -1;
+            } else if(Math.abs(leftHeight - rightHeight) > 1) {
+                return -1;
+            } else {
+                return Math.max(leftHeight, rightHeight) + 1;
+            }
+        }
+        dfs(root);
+        return dfs(root) !== -1;
+    }
+}
