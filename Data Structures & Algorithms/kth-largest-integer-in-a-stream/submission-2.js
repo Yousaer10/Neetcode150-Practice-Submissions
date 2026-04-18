934309e2b24fafe113ff31b7ec83035b591dfad6
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-04-18 09:02:37 +0100

    Add: kth-largest-integer-in-a-stream - submission-2

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-2.js
@@ -0,0 +1,18 @@
+class KthLargest {
+    constructor(k, nums) {
+        this.k = k;
+        this.minHeap = new MinPriorityQueue();
+
+        for(const num of nums) {
+            this.add(num);
+        }
+    }
+
+    add(val) {
+        this.minHeap.enqueue(val);
+        if(this.minHeap.size() > this.k) {
+            this.minHeap.dequeue(val);
+        }
+        return this.minHeap.front();
+    }
+}
