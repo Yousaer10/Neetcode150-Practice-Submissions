c024aed38a2b81ddc5e5282a70834fc44f3011cb
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-04-18 09:51:29 +0100

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    
+    lastStoneWeight(stones) {
+        const maxHeap = new MaxPriorityQueue();
+        for(const stone of stones) {
+            maxHeap.enqueue(stone);
+        }
+
+        while(maxHeap.size() > 1) {
+            const stone1 = maxHeap.dequeue();
+            const stone2 = maxHeap.dequeue(); 
+
+            if(stone1 !== stone2) {
+                maxHeap.enqueue(stone1 - stone2);
+            }
+        }
+        return maxHeap.isEmpty() ? 0 : maxHeap.front();
+    }
+}
