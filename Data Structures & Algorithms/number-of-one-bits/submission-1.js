da97700094770a11a2ffefc9c8e752c20ca12e82
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-04-28 22:53:31 +0100

    Add: number-of-one-bits - submission-1

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-1.js
@@ -0,0 +1,12 @@
+class Solution {
+    hammingWeight(n) {
+        let count = 0;
+        while(n > 0) {
+            if((n & 1) === 1) { // If the last bit is a 1, add to count
+                count++;
+            }
+            n >>>= 1;  // Shift right by 1 to see the next bit
+        }
+        return count;
+    }
+}
