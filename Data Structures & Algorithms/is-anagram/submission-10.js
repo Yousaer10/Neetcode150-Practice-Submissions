f43576b30101a45e8c0254521f8714d97605d181
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-06-06 11:59:22 +0100

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.js
@@ -0,0 +1,20 @@
+class Solution {
+    isAnagram(s, t) {
+        // Do a mapping of the letters for string and frequency
+        if(s.length !== t.length) return false;
+        let count = new Map();
+        for(const char of s) {
+            count.set(char, (count.get(char) ?? 0) + 1);
+        }
+        for(const char of t) {
+            if(!count.has(char)) {
+                return false;
+            }
+            count.set(char, (count.get(char) - 1));
+            if(count.get(char) < 0) {
+                return false;;
+            }
+        }
+        return true
+    }
+}
