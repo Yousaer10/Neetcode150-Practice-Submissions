44c0b1f1a20edd56f4bcc93f485670b4bc47bf5c
Yousaer <125137001+Yousaer10@users.noreply.github.com>
2026-06-14 23:33:25 +0100

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.js
@@ -0,0 +1,32 @@
+class Solution {
+    isValid(s) {
+        let stack = [];
+        const pairs = {
+            ')' : '(',
+            '}' : '{',
+            ']' : '[',
+        };
+        
+        let popped;
+        for(let i = 0; i < s.length; i++) {
+            if(s[i] === '(' || s[i] === '{' || s[i] === '[') {
+                stack.push(s[i]);
+            } else {
+                if(stack.length === 0) {
+                    return false;
+                }
+                popped = stack.pop();
+
+                if(pairs[s[i]] !== popped) {
+                    return false;
+                }
+            }
+
+        }
+        if(stack.length !== 0) {
+            return false
+        } else {
+            return true;
+        }
+    }
+}
